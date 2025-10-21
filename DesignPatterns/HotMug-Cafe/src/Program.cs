b1f0f9c2ae7c6748e4dd52a4538945989ed58223
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-10-21 14:57:30 -0400

    Restructuring the project organisation

---
+++ DesignPatterns/HotMug-Cafe/src/Program.cs
@@ -0,0 +1,27 @@
+﻿
+static void RunLoop()
+{
+    Console.WriteLine("\nPress 'd' to take damage, 'h' to heal, or 'q' to quit:");
+    while (true)
+    {
+        char input = Console.ReadKey(true).KeyChar;
+
+        if (input == 'd')
+        {
+            //player.TakesDamage();
+        }
+        else if (input == 'h')
+        {
+            //player.Heal();
+        }
+        else if (input == 'q')
+        {
+            break;
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("Game Over!");
+}
+
+RunLoop();
