01755de424809a668118f8fd2620aea90b5e67c8
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-10-21 17:10:11 -0400

    Seasoning

--- DesignPatterns/HotMug-Cafe/src/classes/Beverage.cs
+++ DesignPatterns/HotMug-Cafe/src/classes/Beverage.cs
@@ -2,22 +2,22 @@ public abstract class Beverage
 {
   private string? _description; 
   private float gst = 0.05f; 
-  private float qst = 0.975f; 
-
-  public Beverage()
+  private float qst = 0.0975f; 
+  private float extraPrices;
+  public Beverage(int chocolate, int steamedMilk, int whippedCream)
   {
-
+    extraPrices = 0.30f * steamedMilk + 0.6f * chocolate + 1.1f * whippedCream; 
   }
 
   public abstract float Cost();
   public abstract string GetDescription();
   public float Total() 
   {
-    return (gst + qst) * Cost();
+    return (gst + qst) * Cost() + Cost() + extraPrices;
   }
 
   public override string ToString()
   {
-    return $"Café {GetDescription()} vendu: ${Total()}"; 
+    return $"Café {GetDescription()} vendu: {Total():C}"; 
   }
 }
--- DesignPatterns/HotMug-Cafe/src/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ DesignPatterns/HotMug-Cafe/src/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ab9c505cfdf80605c20a3eb9d08b062dd034a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa9d05be9405b5b64b6c16e86124906662cbe9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
